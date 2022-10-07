7e9ae37cd5483f469596658011d172907a8fde38
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-07 17:02:57 +0530

    Added: CalculateDailyEmployeeWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,7 +5,12 @@ randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ]
 then
-echo " Employee is Present "
+echo "Employee is Present "
+empRatePerHr=20;
+empHrs=8;
+Salary=$(($empRatePerHr*$empHrs));
+echo "DailyWage"=$Salary
 else
-echo " Employee is Absent "
+echo "Employee is Absent "
+echo "DailyWage="$Salary
 fi
