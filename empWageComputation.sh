902ec9b6294d877d089d87bf86b922be20ea85cc
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-08 16:14:57 +0530

    Refector: Calculating wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,22 @@ echo " Welcome to Employee wage computation Program"
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
+numWorkingDays=20;
 
+for ((day=1; day<=numWorkingDays; day++))
+do
 randomCheck=$((RANDOM%3));
-
 case $randomCheck in $isFullTime)
-                      empHrs=8;;
-                     $isPartTime)
-                      empHrs=4;;
-                     *)
-                      empHrs=0;;
-esac
+		empHrs=8;;
+		$isPartTime)
+		empHrs=4;;
+		*)
+		empHrs=0;;
+	esac
+	salary=$(($empHrs*$empRatePerHr));
+	totalSalary=$(($totalSalary + $salary));
+done
 
-Salary=$(($empHrs*empRatePerHr))
-echo "Salary=$Salary"
+echo "Employee has earned $totalSalary$ in a month";
