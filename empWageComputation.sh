06884205ef78b8f1b2e01137280960480797dfb5
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-08 16:58:49 +0530

    Refector: Calculate Wages till a condition of total working hours or days is reaced for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,32 @@
 echo " Welcome to Employee wage computation Program"
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
 numWorkingDays=20;
+empRatePerHr=20;
+totalWorkingHour=0;
+day=1;
 
-for ((day=1; day<=numWorkingDays; day++))
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
 do
-randomCheck=$((RANDOM%3));
-case $randomCheck in $isFullTime)
-		empHrs=8;;
-		$isPartTime)
-		empHrs=4;;
-		*)
+	randomCheck=$((RANDOM%3));
+	case $randomCheck in
+		0)
 		empHrs=0;;
-	esac
-	salary=$(($empHrs*$empRatePerHr));
-	totalSalary=$(($totalSalary + $salary));
-done
+                1)
+		empHrs=4;;
+		2)
+		empHrs=8;;
 
-echo "Employee has earned $totalSalary$ in a month";
+esac
+
+	totalWorkingHour=$(($totalWorkingHour+$empHrs))
+
+	if [ $totalWorkingHour -gt 40 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $empHrs))
+		break;
+	fi
+	salary=$(($empRatePerHr*$empHrs))
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
